dcbe3a4f4ec2dba5ed9ad49d383f8266bd51f993
thapakorn <thapakorn_t@cmu.ac.th>
2017-03-20 18:21:33 +0700

    1.4 1.5

---
+++ src/test/java/camt/se234/unittest/dao/UserServiceImplTest.java
@@ -0,0 +1,41 @@
+package camt.se234.unittest.dao;
+import camt.se234.unittest.entity.User;
+import camt.se234.unittest.service.UserServiceImpl;
+
+import org.junit.Rule;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import org.junit.rules.ExpectedException;
+
+import java.time.LocalDate;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+
+
+public class UserServiceImplTest {
+    @Test
+    public void testisAbleToGoToPub(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao = new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.isAbleToGoToPub(userService.login("Prayuth","1234"), LocalDate.of(2017,3,20)), is(true));
+        assertThat(userService.isAbleToGoToPub(userService.login("Tucky","5675"), LocalDate.of(2017,3,20)), is(false));
+        assertThat(userService.isAbleToGoToPub(userService.login("Honey","aabbcc"), LocalDate.of(2017,3,20)), is(false));
+        assertThat(userService.isAbleToGoToPub(userService.login("None","none"), LocalDate.of(2017,3,20)), is(false));
+
+    }
+    @Test
+    public void testgetPubAllowanceUser(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao = new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.getPubAllowanceUser( LocalDate.of(2017,3,20)), hasItems(new User("Prayuth","1234","Tu",
+                LocalDate.of(1979,2,14),"08612345678")));
+    }
+
+
+
+
+
+
+}
